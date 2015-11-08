6caa6efcd3e389fe673191ca5621cd3fc14f92a2
Jonathan Silva <jhonny77111@gmail.com>
2015-11-07 23:52:45 -0430

    Ordenar Facturas
    Mostar ordenados por fecha_entrada

--- protected/models/Facturas.php
+++ protected/models/Facturas.php
@@ -104,6 +104,9 @@ class Facturas extends CActiveRecord
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
+			'sort'=>array(
+				'defaultOrder' => 'fecha_entrada DESC',
+			),
 		));
 	}
 
